1a63c2138951a5493144c3c81191439fb6854272
Mendrika27 <98638405+Mendrika27@users.noreply.github.com>
2022-08-28 16:46:29 +0300

    chore: initial commit

---
+++ src/main/java/com/example/spyapp/SpyAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.spyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpyAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpyAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spyapp/SpyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpyAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
